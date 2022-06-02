e1dc75c78a34268a9edfb3d83b6a74871fda1780
LiilBoo <liltrainer@pm.me>
2022-06-02 21:21:57 +0200

    Dead router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './shared/Pages/home/home.component';
 import { LoginPageComponent } from './shared/Pages/login-page/login-page.component';
 import { Page404Component } from './shared/Pages/page404/page404.component';
 import { PageSignUpComponent } from './shared/Pages/page-sign-up/page-sign-up.component';
+import { TourDetailsComponent } from './shared/Pages/tour-details/tour-details.component';
 // import { SharedModule } from './shared/shared.module';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'login', component: LoginPageComponent},
   {path:'sign-up', component: PageSignUpComponent},
-  {path:'tour-card/:id-tour', component: PageSignUpComponent},
+  {path:'tour-details/:id', component: TourDetailsComponent},
   {path: '404-not-found', component: Page404Component},
   {path: '**' , redirectTo: '/404-not-found'}
 ];
