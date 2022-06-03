0583e7c40eb13836ac4807709353248d721783c0
LiilBoo <liltrainer@pm.me>
2022-06-03 21:45:09 +0200

    HTML Semantic fixing

---
+++ src/app/OrganisatorModule/organisator.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrganisatorRoutingModule } from './organisator-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OrganisatorRoutingModule
+  ]
+})
+export class OrganisatorModule { }
